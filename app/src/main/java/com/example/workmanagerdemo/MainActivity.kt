4202a425b550d3597f2246eec6b7d33ccfb84cbb
abubaker <mohammad_abubaker@yahoo.com>
2021-12-14 23:27:14 +0500

    Working...

--- app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
@@ -18,10 +18,29 @@ class MainActivity : AppCompatActivity() {
 
         // Event: One Time Request
         binding.btnOneTimeRequest.setOnClickListener {
+
+            /**
+             * SetRequires ****** (Examples)
+             * ======================
+             * 1. Network Type
+             * 2. Batter Not Low
+             * 3. Charging
+             * 4. Device Idle
+             * 5. Storage Not Low
+             *
+             */
+
             val oneTimeRequestConstraints = Constraints.Builder()
+
+                // 1. Device is NOT Charging
                 .setRequiresCharging(false)
+
+                // 2. Internet is Connected
                 .setRequiredNetworkType(NetworkType.CONNECTED)
+
+                // Due to our two defined constraints, this request will only work if those requirements are met.
                 .build()
+
         }
 
     }
