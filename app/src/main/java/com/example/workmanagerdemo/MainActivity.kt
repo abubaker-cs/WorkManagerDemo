50960508e5ac4571ee01add111e561cebbbebf6a
abubaker <mohammad_abubaker@yahoo.com>
2021-12-15 16:18:37 +0500

    Taking break

--- app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.Observer
 import androidx.work.*
 import androidx.work.WorkInfo.*
 import com.example.workmanagerdemo.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -129,6 +130,49 @@ class MainActivity : AppCompatActivity() {
                 })
         }
 
+        // Event: Preodic R
+// TODO Step 5: Assign the click even for Periodic Work Request. And Enqueue the worker class.,
+        binding.btnPeriodicRequest.setOnClickListener {
+
+            /**
+             * Constraints ensure that work is deferred until optimal conditions are met.
+             *
+             * A specification of the requirements that need to be met before a WorkRequest can run.
+             * By default, WorkRequests do not have any requirements and can run immediately.
+             * By adding requirements, you can make sure that work only runs in certain situations
+             * - for example, when you have an unmetered network and are charging.
+             */
+            // For more details visit the link https://medium.com/androiddevelopers/introducing-workmanager-2083bcfc4712
+            val periodicRequestConstraints = Constraints.Builder()
+                .setRequiresBatteryNotLow(false)
+                .setRequiresCharging(false)
+                .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
+                .build()
+
+            // Create an Periodic Work Request
+            /**
+             * You can use any of the work request builder that are available to use.
+             * We will you the PeriodicWorkRequestBuilder as we want to execute the code periodically.
+             *
+             * The minimum time you can set is 15 minutes. You can check the same on the below link.
+             * https://developer.android.com/reference/androidx/work/PeriodicWorkRequest
+             *
+             * You can also set the TimeUnit as per your requirement. for example SECONDS, MINUTES, or HOURS.
+             */
+            // setting period to 15 Minutes
+            val periodicWorkRequest =
+                PeriodicWorkRequest.Builder(PeriodicRequestWorker::class.java, 15, TimeUnit.MINUTES)
+                    .setConstraints(periodicRequestConstraints)
+                    .build()
+
+            /* Enqueue a work, ExistingPeriodicWorkPolicy.KEEP means that if this work already exists, it will be kept
+        if the value is ExistingPeriodicWorkPolicy.REPLACE, then the work will be replaced */
+            WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+                "Periodic Work Request",
+                ExistingPeriodicWorkPolicy.KEEP,
+                periodicWorkRequest
+            )
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/workmanagerdemo/PeriodicRequestWorker.kt
@@ -0,0 +1,42 @@
+package com.example.workmanagerdemo
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import java.text.SimpleDateFormat
+import java.util.*
+
+// Create a worker class for Periodic Work Request.
+class PeriodicRequestWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+
+
+    override fun doWork(): Result {
+
+        // TODO Step 6: Print the date in log when the function is called.
+        // START
+        val date = getDate(System.currentTimeMillis())
+        Log.i("Periodic WorkRequest", "doWork Execution DateTime: $date")
+
+        return Result.success()
+        // END
+    }
+
+    // reate a function to get the date from the Milliseconds.
+    /**
+     * A function to get the date from the Milliseconds.
+     *
+     * @param milliSeconds
+     */
+    private fun getDate(milliSeconds: Long): String {
+
+        // Create a DateFormatter object for displaying date in specified format.
+        val formatter = SimpleDateFormat("dd/MM/yyyy hh:mm:ss.SSS", Locale.getDefault())
+
+        // Create a calendar object that will convert the date and time value in milliseconds to date.
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = milliSeconds
+        return formatter.format(calendar.time)
+    }
+
+}
